77ea5fdc45f31409856d74fdc54ee6d070e89d75
Thanaphon06 <thanaphonsitthimat06@gmail.com>
2022-06-17 15:47:08 +0700

    java fist class

---
+++ makga.java
@@ -0,0 +1,10 @@
+public class makga {
+ public static void main(String[] args) {
+    int num = 50;
+    if( num > 60){
+        System.out.print("True yuhu");
+    }else{
+        System.out.print("meetung");
+    }
+ }   
+}
---
+++ test.java
@@ -0,0 +1,8 @@
+public class test{
+    public static void main(String[] args) {
+        //! println คือการขึ้นบรรทัดใหม่อัตโนมัติ
+        System.out.println("Hello moto yoo ehi");
+        System.out.print("Yoo print derr");
+        
+    }
+}

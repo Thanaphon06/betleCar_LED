dc491eac04274fca4d51cb031cb26acba389b8f2
Thanaphon06 <68720136+Thanaphon06@users.noreply.github.com>
2022-06-24 14:44:52 +0700

    Add files via upload

--- userin.java
+++ userin.java
@@ -21,6 +21,26 @@ public class userin {
         System.out.println(year +" olds");
         System.out.println("you weath " +weigth);
 
+        
+
+        switch (year) {
+            case 7: System.out.println("Gread 4");
+                break;
+                case 8: System.out.println("Gread 5");
+                break;
+                case 9: System.out.println("Gread 6");
+                break;
+                case 10: System.out.println("Gread 7");
+                break;
+                case 11: System.out.println("Gread 8");
+                break;
+                case 12: System.out.println("Gread 9");
+                break;
+              
+            default: System.out.println("Pleas try agian");
+                break;
+        }
+
     }
 
 }

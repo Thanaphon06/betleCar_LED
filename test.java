d95c93d3ed3290cea67f08b933614b73cbf4c6c8
Thanaphon06 <68720136+Thanaphon06@users.noreply.github.com>
2023-04-11 11:11:33 +0700

    Delete test.java

--- test.java
+++
@@ -1,8 +0,0 @@
-public class test{
-    public static void main(String[] args) {
-        //! println คือการขึ้นบรรทัดใหม่อัตโนมัติ
-        System.out.println("Hello moto yoo ehi");
-        System.out.print("Yoo print derr");
-        
-    }
-}
